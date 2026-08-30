20b2032ade75eec53552724a05a81698358f5e37
TahirMustafa-NO-ONE <tahirmustafa161123@gmail.com>
2026-08-30 18:45:52 +0500

    product of array except self

---
+++ Arrays/17-prodArrExcSelf.js
@@ -0,0 +1,20 @@
+function productOfArrayExceptSelf(arr){
+    const n = arr.length;
+    const result = new Array(n).fill(1);
+
+    let prefix = 1;
+    for(let i=0; i<n; i++){
+        result[i] = prefix;
+        prefix *= arr[i];
+    }
+
+    let suffix = 1;
+    for(let i=n-1; i>=0; i--){
+        result[i] *= suffix;
+        suffix *= arr[i];
+    }
+
+    return result;
+}
+
+console.log(productOfArrayExceptSelf([1,2,3,4,5,6]));
