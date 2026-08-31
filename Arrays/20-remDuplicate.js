f89624c53ae751c9f885ea762af5cd10de4844ef
TahirMustafa-NO-ONE <tahirmustafa161123@gmail.com>
2026-08-31 10:54:14 +0500

    keeping only the unique element

--- Arrays/20-remDuplicate.js
+++ Arrays/20-remDuplicate.js
@@ -19,7 +19,7 @@ console.log(remDup1([2,4,2,1,3,5,6,4,3,6,3,2]));
 
 function remDup2(arr){
     const sortArr = arr.sort((a, b) => (a-b));
-    console.log(sortArr);
+    // console.log(sortArr);
     const newArr = [];
     for(let i=0; i<sortArr.length; i++){
         if(newArr.length>0 && newArr[newArr.length-1] === sortArr[i]){
@@ -32,3 +32,19 @@ function remDup2(arr){
 }
 
 console.log(remDup2([2,4,2,1,3,5,6,4,3,6,3,2]));
+
+// removing element that appears more than 1 time
+
+function remDup3(arr){
+    const count = {};
+
+    for(let i=0; i<arr.length; i++){
+        count[arr[i]] = (count[arr[i]] || 0) + 1;
+    }
+
+    console.log(count);
+
+    return arr.filter(item => count[item] === 1);
+}
+
+console.log(remDup3([2,4,2,1,3,5,6,4,3,6,3,2]));
