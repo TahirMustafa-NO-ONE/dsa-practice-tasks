4c518d95a4225a6e95d3f0f2ee8e0540045c6445
TahirMustafa-NO-ONE <tahirmustafa161123@gmail.com>
2026-09-07 18:38:32 +0500

    find pivot element in array

---
+++ Arrays/29-pivotIndex.js
@@ -0,0 +1,25 @@
+function findPivot(arr){
+    const n = arr.length;
+    let totalSUm = 0;
+
+    for(let i=0; i<n; i++){
+        totalSUm += arr[i];
+    }
+
+    let leftSum = 0;
+
+    for(let i=0; i<n; i++){
+        let rightSum = totalSUm - leftSum - arr[i];
+
+        if(leftSum === rightSum){
+            return [i, arr[i]];
+        }
+
+        leftSum += arr[i];
+
+    }
+
+    return -1;
+}
+
+console.log(findPivot([2,4,3,5,3,12,2]));
