5e68eabeadd1b5c67f9dfe4a6abc6fac955c81c2
Tahir Mustafa <162949902+TahirMustafa-NO-ONE@users.noreply.github.com>
2026-09-13 10:08:21 +0500

    Create maxSumOfSubArrWithSum0.js

---
+++ Arrays/maxSumOfSubArrWithSum0.js
@@ -0,0 +1,27 @@
+const are = [15, -2, 2, -8, 1, 7, 10];
+
+function maxArray(arr){
+
+const map = new Map();
+
+map.set(0, -1);
+let prefixSum = 0;
+let maxLen = 0;
+
+for(let i=0; i<arr.length; i++) {
+    prefixSum += arr[i];
+    if(map.has(prefixSum)){
+       let length = seen.get(prefixSum) - i;
+       maxLen = Math.max(maxLen, length);
+       
+}
+else{
+     map.set(prefixSum, i);
+}
+}
+
+return maxLen;
+
+}
+
+console.log(maxArray(arr));
